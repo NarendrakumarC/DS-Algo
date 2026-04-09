b5d33e9d2d8052fcbff7d4b29b1e99859f9765c7
Narendra kumar C <narendrakumarc500@gmail.com>
2026-04-09 23:16:16 +0530

    Time: 7 ms (74.41%) | Memory: 49.4 MB (87.63%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -5,9 +5,7 @@ class Solution {
             char []ch = st.toCharArray();
             Arrays.sort(ch);
             String key = new String(ch);
-            if(!hm.containsKey(key)){
-                hm.put(key, new ArrayList<>());
-            }
+            hm.putIfAbsent(key,new ArrayList<>());
             hm.get(key).add(st);
         }
         return new ArrayList<>(hm.values());
