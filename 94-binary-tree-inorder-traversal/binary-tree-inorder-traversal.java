5bf077c7445ac22236fec5172a2db3737e5ac23f
Narendra kumar C <narendrakumarc500@gmail.com>
2026-04-30 12:12:03 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (69.42%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -14,17 +14,42 @@
  * }
  */
 class Solution {
-    List<Integer> result = new ArrayList<>();
+     List<Integer> ans;
     public List<Integer> inorderTraversal(TreeNode root) {
-            printInOrder(root);
-           return result; 
+         ans = new ArrayList<>();
+        morrisTraversal(root);
+        return ans;
     }
+    private void morrisTraversal(TreeNode node) {
+		TreeNode curr = node;
+		while(curr!=null) {
+			TreeNode lnode = curr.left;
+			if(lnode == null) {
+				ans.add(curr.val);
+				curr = curr.right;
+			}else {
+				// find rightMost node
+				TreeNode rmnode = getRightMostNode(lnode,curr);
+				if(rmnode.right == null) {
+					// case of making thread and moving toward left
+                    rmnode.right = curr;
+                    curr = curr.left;
+				}else {
+                    // in this case, rmnode.right is curr itself
+                    // print -> break the thread -> move toward right 
+                    ans.add(curr.val);
+                    rmnode.right = null;
+                    curr = curr.right;
+                }
+			}
+		}
+	}
 
-    private void printInOrder(TreeNode root){
-        // base case
-        if(root == null) return;
-        printInOrder(root.left);
-        result.add(root.val);
-        printInOrder(root.right);
+   private  TreeNode getRightMostNode(TreeNode lnode, TreeNode curr) {
+        TreeNode temp = lnode;
+        while(temp.right != null && temp.right != curr) {
+            temp = temp.right;
+        }
+        return temp;
     }
 }
