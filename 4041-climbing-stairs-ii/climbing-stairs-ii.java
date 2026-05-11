2ee8d6b4d58581d7d6b523843792451e88c0249b
Narendra kumar C <narendrakumarc500@gmail.com>
2026-05-11 15:18:29 +0530

    Time: 7 ms (68.53%) | Memory: 117 MB (64.40%) - LeetSync

--- 4041-climbing-stairs-ii/climbing-stairs-ii.java
+++ 4041-climbing-stairs-ii/climbing-stairs-ii.java
@@ -1,9 +1,17 @@
 class Solution {
     public int climbStairs(int n, int[] costs) {
         int dp[] = new int[n+1];
-        Arrays.fill(dp, -1);
-        int res = minCost(0, n, dp, costs);
-        return res;
+        dp[0] =0;
+        for(int j=1;j<=n; j++){
+            dp[j] = Integer.MAX_VALUE;
+            if(j-1 >= 0) 
+                dp[j] = Math.min(dp[j], dp[j-1]+costs[j-1]+1);
+            if(j-2 >= 0)
+                dp[j] = Math.min(dp[j], dp[j-2]+costs[j-1]+4);
+            if(j-3>=0)
+                dp[j] = Math.min(dp[j], dp[j-3]+costs[j-1]+9);    
+        }
+        return dp[n];
     }
     private static int minCost(int i, int n, int []dp, int []cost){
         //base case
