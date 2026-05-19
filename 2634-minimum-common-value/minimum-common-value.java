d91823ae8ab7dbfc0ec7b3792ef8db4e222443ca
Narendra kumar C <narendrakumarc500@gmail.com>
2026-05-19 13:16:08 +0530

    Time: 1 ms (98.36%) | Memory: 80.3 MB (79.53%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.java
+++ 2634-minimum-common-value/minimum-common-value.java
@@ -2,7 +2,13 @@ class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
         int i=0; int j =0;
         int l1 = nums1.length;
-        int l2= nums2.length;
+        int l2 = nums2.length;
+        // Invalid
+        if (nums1[l1 - 1] < nums2[0] || nums2[l2 - 1] < nums1[0]) {
+            return -1;
+        }
+        // use this approach only if array is sorted 
+        //two pointers, i and j, at the start of both arrays
         while(i<l1 && j<l2){
             if(nums1[i] == nums2[j]){
                 return nums1[i];
@@ -13,7 +19,6 @@ class Solution {
                     j++;
                 }
             }
-            
         }
         return -1;
     }
