56986681e836038575d8806b020f85bff4ddadfd
Narendra kumar C <narendrakumarc500@gmail.com>
2026-05-19 14:34:15 +0530

    Time: 1 ms (1.08%) | Memory: 43 MB (47.16%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.java
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.java
@@ -13,10 +13,34 @@ class Solution {
         if(head == null || head.next == null){
             return head;
         }
-       ListNode second = head.next;
-       head.next = swapPairs(second.next);
-       second.next = head;
-
-        return second;
+       // Convert Linkedlist to arraylist and perform swap using third variable, 
+       // then create new node with values from arraylist and return it
+        List<Integer> nums = new ArrayList<>();
+        ListNode temp = head;
+        while(temp != null){
+            nums.add(temp.val);
+            temp = temp.next;
+        }
+        for(int i=0; i<nums.size(); i+=2){
+            int val1= nums.get(i);
+            int val2=0;
+            if((i+1) < nums.size()){
+                val2 = nums.get(i+1);
+                int k = val1;
+                nums.set(i, val2);
+                nums.set(i+1, k);
+            }
+        }
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = dummy;
+        for(int i=0;i<nums.size();i++){
+            ListNode nn = new ListNode(nums.get(i));
+            //create link to newly create node
+            curr.next = nn;
+            //move the pointer to newly create node
+            //move to attached node
+            curr = nn;
+        }
+        return dummy.next;
     }
 }
