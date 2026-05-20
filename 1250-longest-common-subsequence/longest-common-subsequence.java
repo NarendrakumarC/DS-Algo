22336c39a6b217642dc3dc2510f8d3f25b2dfd08
Narendra kumar C <narendrakumarc500@gmail.com>
2026-05-20 23:50:47 +0530

    Time: 20 ms (78.26%) | Memory: 54 MB (50.20%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,28 +1,27 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
+        // DP:Tabulation
         int n = text1.length();
         int m = text2.length();
-        int dp[][] = new int[n][m];
-        // Loop through each row and fill it with -1
-      /*  for (int[] row : dp) {
-            Arrays.fill(row, -1);
-        } */
-        Arrays.stream(dp).forEach(row -> Arrays.fill(row, -1));
-        // n-1 and m-1 are last index
-        return lcs(text1, text2, n-1, m-1, dp);
-    }
-    private int lcs(String s1, String s2, int i, int j, int [][]dp){
-            // if both indexes are exhausted
-            // base case
-            if(i<0 || j<0) return 0;
-            if(dp[i][j] != -1){
-                return dp[i][j];
+        int [][]dp = new int[n+1][m+1];
+        // fill 0th row and 0th col with zero values
+        // 1. Fill the entire 0th row with 0
+        Arrays.fill(dp[0],0);
+        // 2. Fill the 0th column of every row with 0
+        for(int row=0;row<dp.length;row++){
+            dp[row][0]=0;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(text1.charAt(i-1) == text2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1] +1;
+                }else{
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
             }
-            if(s1.charAt(i) == s2.charAt(j)){
-                dp[i][j] = lcs(s1, s2, i-1, j-1, dp) +1;
-            }else{
-                dp[i][j] = Math.max(lcs(s1, s2, i, j-1, dp), lcs(s1, s2, i-1, j, dp));
-            }
-            return dp[i][j];
+        }
+// because LCS lies in last indext n-1,m-1
+        return dp[n][m];
     }
+    
 }
