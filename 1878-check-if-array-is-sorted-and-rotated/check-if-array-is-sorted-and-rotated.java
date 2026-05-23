fea916c482e441e7406693cbcab18b65e0d6fad4
Narendra kumar C <narendrakumarc500@gmail.com>
2026-05-24 00:32:46 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (11.32%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -1,37 +1,14 @@
 class Solution {
     public boolean check(int[] nums) {
+        int count = 0;
         int n = nums.length;
-       
-    for (int x = 0; x < n; x++) {
-            int[] B = nums.clone(); // avoid modifying original
-            rotateK(B, n, x);
-            boolean sorted = true;
-            for (int i = 0; i < n - 1; i++) {
-                if (B[i] > B[i + 1]) {
-                    sorted = false;
-                    break;
-                }
+        
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > nums[(i + 1) % n]) {
+                count++;
             }
-            if (sorted) return true;
         }
-        return false;
-    }
-    private int[] rotateK(int[] arr, int N, int K){
-        K=K%N;
-        arr = reverseInRange(arr, 0, N-1);
-        arr = reverseInRange(arr, 0, K-1);
-        arr = reverseInRange(arr, K, N-1);
-        return arr;
-    }
-    private int[] reverseInRange(int arr[], int L, int R){
-        int i=L; int j=R;
-        while(i<j){
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-            i++;
-            j--;
-        }
-        return arr;
+        
+        return count <= 1;
     }
 }
