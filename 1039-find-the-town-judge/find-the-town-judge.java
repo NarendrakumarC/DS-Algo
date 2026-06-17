b707484859bc65b1aaae9b8d981055b1a44e3670
Narendra kumar C <narendrakumarc500@gmail.com>
2026-06-17 18:43:58 +0530

    Time: 3 ms (72.00%) | Memory: 53.7 MB (32.95%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.java
+++ 1039-find-the-town-judge/find-the-town-judge.java
@@ -1,21 +1,17 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        int []score = new int[n+1];
-        // Step 1: process trust relationship
-        for(int []t : trust){
-            int a = t[0];
-            int b = t[1];
-
-            score[a]--; //a trust someone
-            score[b]++; //b is truste by someone
-        }
-        // find the person with score n-1
-        for(int i=1;i<=n;i++){
-            if(score[i]== n-1){
+       int []indegree = new int[n+1];
+       int []outdegree = new int[n+1];
+       for(int []t : trust){
+            indegree[t[1]]++; // a trust someone
+            outdegree[t[0]]++; // b is trusted
+       }
+       for(int i=1;i<=n;i++){
+            if(indegree[i]==n-1 && outdegree[i]==0){
                 return i;
             }
-        }
-        return -1;
+       }
+       return -1;
     }
 
 }
