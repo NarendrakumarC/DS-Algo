669de2f95cbf7407649a9aef6546433468f9a490
Narendra kumar C <narendrakumarc500@gmail.com>
2026-07-18 22:47:18 +0530

    Time: 3 ms (59.85%) | Memory: 44.4 MB (65.67%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        if(s.isEmpty()){
+            return true;
+        }
+        int i=0;
+        int j=s.length()-1;
+        while(i<=j){
+            char currFirst = s.charAt(i);
+            char currLast = s.charAt(j);
+            if(!Character.isLetterOrDigit(currFirst)){
+                i++;
+            }else if(!Character.isLetterOrDigit(currLast)){
+                j--;
+            }else{
+                if(Character.toLowerCase(currFirst) !=Character.toLowerCase(currLast)){
+                    return false;
+                }
+                i++;
+                j--;
+            }
+        }
+         return true;
+    }
+}
